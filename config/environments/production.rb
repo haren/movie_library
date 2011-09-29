674a83897f31ec5f0303d78a5f8bd5df07a32029
Lukasz Harezlak <lukasz.harezlak@gmail.com>
2011-09-29 12:48:02 +0200

    Added precompilation to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,7 @@ MovieLibrary::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+
+	config.assets.precompile += ['movie.css']
 end
