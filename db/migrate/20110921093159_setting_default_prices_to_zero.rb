127bbf07a505985095708b8c545078df865d10c4
Lukasz Harezlak <lukasz.harezlak@gmail.com>
2011-09-29 12:26:51 +0200

    Migrations modified

--- db/migrate/20110921093159_setting_default_prices_to_zero.rb
+++ db/migrate/20110921093159_setting_default_prices_to_zero.rb
@@ -1,6 +1,6 @@
 class SettingDefaultPricesToZero < ActiveRecord::Migration
   def up
-		change_column :movies, :price, :decimal, :default => 0, :precision => 8, :scale => 2
+		add_column :movies, :price, :decimal, :default => 0, :precision => 8, :scale => 2
 		change_column :series, :price, :decimal, :default => 0, :precision => 8, :scale => 2
 		change_column :episodes, :price, :decimal, :default => 0, :precision => 8, :scale => 2
   end
