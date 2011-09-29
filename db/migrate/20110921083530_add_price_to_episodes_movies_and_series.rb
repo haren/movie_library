3cc1f59965c7a843a7965b824fd6e20498af5371
Lukasz Harezlak <lukasz.harezlak@gmail.com>
2011-09-29 12:24:54 +0200

    Migrations fixed

--- db/migrate/20110921083530_add_price_to_episodes_movies_and_series.rb
+++ db/migrate/20110921083530_add_price_to_episodes_movies_and_series.rb
@@ -2,10 +2,12 @@ class AddPriceToEpisodesMoviesAndSeries < ActiveRecord::Migration
   def self.up
 		add_column :episodes, :price, :decimal, :precision => 8, :scale => 2
 		add_column :series, :price, :decimal, :precision => 8, :scale => 2
+		add_column :movies, :price, :decimal, :precision => 8, :scale => 2
   end
 
 	def self.down
 		remove_column :episodes, :price
 		remove_column :series, :price
+		remove_column :movies, :price
 	end
 end
